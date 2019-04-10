c1408e838edc23a6c035bb0d6544863df64b73c5
Mickael Alliel <mickael@rookout.com>
2019-04-10 14:34:13 +0300

    Update frame inspect code

--- frame_inspect.py
+++ frame_inspect.py
@@ -1,20 +1,15 @@
-import sys
-
-
-def tracer(frame, event, arg):
-    locals = frame.f_locals
-    if locals:
-        print 'locals variables: ', locals
-    return tracer
-
+import inspect
 
 def a():
-    print 'in a()'
-    mystr = "mystr"
-    mydict = {"mykey": "myvalue"}
-    mylist = [1, 2, 3]
-    return mystr * 2
+  print 'in a()'
+  mystr = "mystr"
+  frame = inspect.currentframe()
+  print "local vars:", frame.f_locals
+  mydict = {"mykey": "myvalue"}
+  mylist = [1, 2, 3]
+  frame = inspect.currentframe()
+  print "local vars:", frame.f_locals
+  print "frame info:", inspect.getframeinfo(frame)
+  return mystr * 2
 
-
-sys.settrace(tracer)
 a()
